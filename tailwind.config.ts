1e2a7219433bfea14166a0711b51ee341c820d22
Reza Mousavi <MRez321@gmail.com>
2026-06-24 01:45:05 -0700

    commented tailwind animate

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,2 +1,2 @@
 import type { Config } from "tailwindcss";
-export default { content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"], theme: { extend: {} }, plugins: [require("tailwindcss-animate")] } satisfies Config;
+// export default { content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"], theme: { extend: {} }, plugins: [require("tailwindcss-animate")] } satisfies Config;
