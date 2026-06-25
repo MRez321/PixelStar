c62d0ce6b7013868eb938c6c9a1911ac145b9aae
Reza Mousavi <MRez321@gmail.com>
2026-06-25 13:08:44 -0700

    added a placeholder logo

--- components/layout/global-nav.tsx
+++ components/layout/global-nav.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image';
 
+import Logo from '@/assets/pixelstarai - Copy.jpg'
+
 export default function GlobalNavigation() {
   return (
     <>
       <nav className="flex items-center justify-between flex-row w-full ">
         <ul className="flex items-center justify-between flex-row w-full ">
           <li>
-            <Image src='' alt='' />
+            <Image src={Logo} alt='pixel star' className='w-8 h-8' />
           </li>
           <li>1</li>
           <li>2</li>
